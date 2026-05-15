59f19fefc45e9db7b85b9c2e5ade5aa83d4ea74b
ezafrir <emmazafrir@gmail.com>
2026-05-15 17:59:01 -0400

    remove tests, login pages, course deliverables slop

--- testing-deliverables/cucumber.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  default: {
-    paths:   ["features/**/*.feature"],
-    require: ["features/step_definitions/**/*.js"],
-    format:  ["progress-bar", "html:reports/cucumber-report.html"],
-    timeout: 20000
-  }
-};
